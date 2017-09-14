248f688bcf8eacc3aca84e78fae144abd39d0b96
Eric Abell <ericabell@users.noreply.github.com>
2017-09-14 11:25:10 -0500

    Finished writing object to file (serialize an object)

---
+++ src/Cars.java
@@ -0,0 +1,23 @@
+public class Cars {
+    int year;
+    String make;
+    String model;
+
+    public Cars(int year, String make, String model) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
+    public static void writeObjectToFile(Cars car) {
+        try {
+            File file = new File("output.txt");
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(car.getMake() + "\n");
+            fileWriter.write(car.getModel() + "\n");
+            fileWriter.write(car.getYear() + "\n");
+            fileWriter.close();
+
+        } catch( IOException ex ) {
+            ex.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	    Cars car1 = new Cars(2017, "Ford", "Mustang");
+
+	    writeObjectToFile(car1);
     }
 }
